0a2a031704a4bbc87192216aba79ba15c717bd77
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2020-11-16 11:27:13 +0100

    adds jdbc.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
+# syntax=docker/dockerfile:experimental
 FROM maven:3.6.3-jdk-11 AS builder
 COPY MyApp /MyApp
-RUN cd /MyApp && mvn package
+RUN --mount=type=cache,target=/root/.m2 \
+    cd /MyApp && mvn --batch-mode clean package
+
 
 FROM adoptopenjdk/openjdk11:jre-11.0.9_11-ubuntu 
-COPY --from=builder /MyApp/target/MyApp-1.0-SNAPSHOT.jar /MyApp.jar
+
+COPY --from=builder /MyApp/target/MyApp-*-jar-with-dependencies.jar /MyApp.jar
 CMD java -cp /MyApp.jar fr.univtln.bruno.samples.App
--- MyApp/src/main/java/fr/univtln/bruno/samples/App.java
+++ MyApp/src/main/java/fr/univtln/bruno/samples/App.java
@@ -1,13 +1,50 @@
 package fr.univtln.bruno.samples;
 
+import lombok.extern.java.Log;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@Log
+public class App {
+    private static Properties properties = new Properties();
+
+    public static void loadProperties(String propFileName) throws IOException {
+        InputStream inputstream = App.class.getClassLoader().getResourceAsStream(propFileName);
+        if (inputstream == null) throw new FileNotFoundException();
+        properties.load(inputstream);
     }
+
+    static {
+        try {
+            loadProperties("app.properties");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String s) {
+        return properties.getProperty(s);
+    }
+
+    public static void main(String[] args) throws SQLException, InterruptedException {
+
+        log.info("Starting...");
+        Thread.sleep(2000);
+
+        Connection connection = DBCPDataSource.getConnection();
+        DatabaseMetaData dbmd = connection.getMetaData();
+        log.info("Connected to :"+dbmd.getDatabaseProductName()+" "+dbmd.getDatabaseProductVersion());
+
+    }
+
 }
